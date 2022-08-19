412fef3de029df3cb1f461a675ff8adac77cf6ad
Jorman Ortega <jormanortega34@gmail.com>
2022-08-19 17:08:26 -0400

    Comprimidas las imagenes para optimizacion de la web

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,12 @@
-import homeHero from "./home_hero.png";
-import homeCards from "./home_cards.png";
-import feature from "./feature.png";
+import homeHero from "./home_hero.webp";
+import homeCards from "./home_cards.webp";
+import feature from "./feature.webp";
 import expo from "./expo.png";
 import expo02 from "./expo02.png";
 import javascript from "./javascript.png";
 import react from "./react.png";
-import scene from "./scene.png";
-import mockup from "./mockup.png";
+import scene from "./scene.webp";
+import mockup from "./mockup.webp";
 
 export default {
   homeHero,
